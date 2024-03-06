089f35ac3c39035d10c544770c8efba250dccfc2
Simone <150595274+simonetimi@users.noreply.github.com>
2024-03-06 09:44:38 +0100

    Complete exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,9 +1,16 @@
 import { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '123456789' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+  ]);
   const [newName, setNewName] = useState('');
   const [newPhone, setNewPhone] = useState('');
+  const [searchInput, setSearchInput] = useState('');
+  const [found, setFound] = useState('');
 
   const handleOnChangeName = (event) => {
     setNewName(event.target.value);
@@ -27,9 +34,20 @@ const App = () => {
     setPersons([...persons, { name: newName, number: newPhone }]);
   };
 
+  const handleOnChangeSearch = (event) => {
+    const search = event.target.value;
+    setSearchInput(search);
+    setFound(persons.filter((person) => person.name.toLowerCase().includes(search.toLowerCase())));
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        search by name:{' '}
+        <input type="text" name="name" value={searchInput} onChange={handleOnChangeSearch} />
+      </div>
+      <hr />
       <form onSubmit={handleOnSubmit}>
         <div>
           name: <input type="text" name="name" value={newName} onChange={handleOnChangeName} />
@@ -43,11 +61,17 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <li key={person.name}>
-          {person.name}: {person.number}
-        </li>
-      ))}
+      {searchInput === ''
+        ? persons.map((person) => (
+            <li key={person.name}>
+              {person.name}: {person.number}
+            </li>
+          ))
+        : found.map((person) => (
+            <li key={person.name}>
+              {person.name}: {person.number}
+            </li>
+          ))}
     </div>
   );
 };
