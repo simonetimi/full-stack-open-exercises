5d510ee7b64764f71a4c225870edb86e9a72ee55
Simone <150595274+simonetimi@users.noreply.github.com>
2024-03-08 12:52:24 +0100

    Complete exercise 3.7

---
+++ part3/README.md
@@ -0,0 +1,5 @@
+## Solutions for the exercises of part 3 - Full Stack Open course
+
+### Exercises
+
+- phonebook-backend: 3.1-3.8
--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const data = require('./data.js');
 let contacts = data.contacts;
@@ -10,6 +11,8 @@ const PORT = 3001;
 app.listen(PORT);
 
 app.use(express.json());
+
+app.use(morgan('tiny'));
 
 app.get('/api/persons', (request, response) => {
   response.json(data);
