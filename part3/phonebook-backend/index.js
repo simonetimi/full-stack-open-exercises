15f9ba085e75c88abf7097c3d611deaa1b137917
Simone <150595274+simonetimi@users.noreply.github.com>
2024-03-08 10:45:26 +0100

    Complete exercise 3.4

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 const data = require('./data.js');
-const contacts = data.contacts;
+let contacts = data.contacts;
 
 const PORT = 3001;
 app.listen(PORT);
@@ -27,4 +27,16 @@ app.get('/api/persons/:id', (request, response) => {
     });
   }
   response.json(contact);
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const numbOfContacts = contacts.length;
+  contacts = contacts.filter((contact) => contact.id !== id);
+  if (contacts.length === numbOfContacts) {
+    return response.status(404).json({
+      error: 'Contact not found',
+    });
+  }
+  response.status(204).end();
 });
