8a6f23b9cf21ff2ebdcbd0fbbdc31848750915b5
Simone <150595274+simonetimi@users.noreply.github.com>
2024-03-08 10:24:50 +0100

    Complete exercise 3.2

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -2,11 +2,18 @@ const express = require('express');
 const app = express();
 
 const data = require('./data.js');
-const contacts = data.notes;
+const contacts = data.contacts;
 
 const PORT = 3001;
 app.listen(PORT);
 
 app.get('/api/persons', (request, response) => {
   response.json(data);
+});
+
+app.get('/api/info', (request, response) => {
+  const numberOfPeople = contacts.length;
+  const now = new Date();
+  const info = `<p>Phonebook has ${numberOfPeople} contacts</p><p>${now}</p>`;
+  response.send(info);
 });
