c04abc26d32c4e93bae714429ad202f3dbf1d9cb
Simone <150595274+simonetimi@users.noreply.github.com>
2024-03-10 13:24:35 +0100

    Complete exercise 4.13

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -15,11 +15,35 @@ blogsRouter.get('/', async (request, response, next) => {
   }
 });
 
+blogsRouter.get('/:id', async (request, response, next) => {
+  try {
+    const id = request.params.id;
+    const blog = await Blog.findById(id);
+    response.json(blog);
+  } catch (error) {
+    next(error);
+  }
+});
+
 blogsRouter.post('/', async (request, response, next) => {
   try {
     const blog = new Blog(request.body);
     const result = await blog.save();
     response.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+blogsRouter.delete('/:id', async (request, response, next) => {
+  try {
+    const id = request.params.id;
+    const result = await Blog.findByIdAndDelete(id);
+    if (result) {
+      response.status(204).end();
+    } else {
+      response.status(404).json({ message: 'Blog not found' });
+    }
   } catch (error) {
     next(error);
   }
