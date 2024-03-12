51e8df07c109318d8c29bde539c494c06d382ab7
Simone <150595274+simonetimi@users.noreply.github.com>
2024-03-12 13:07:59 +0100

    Complete exercise 5.14

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,7 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import Blog from './Blog';
+import userEvent from '@testing-library/user-event';
 
-test('renders content', () => {
+/*
+test('renders blog without url and likes', () => {
   const blog = {
     author: 'Me',
     title: 'This is the title',
@@ -17,4 +19,30 @@ test('renders content', () => {
   expect(element).toBeDefined();
   expect(url).toBeNull;
   expect(likes).toBeNull;
+});
+*/
+
+test('renders blog with url and likes when button is pressed', async () => {
+  const blog = {
+    author: 'Me',
+    title: 'This is the title',
+    user: { username: 'User' },
+    url: 'http',
+    likes: 3,
+  };
+
+  render(<Blog blog={blog} username="Nit" />);
+
+  const url = screen.getByText('Blog url: http');
+  const likes = screen.getByText('Likes: 3');
+
+  expect(url).not.toBeVisible();
+  expect(likes).not.toBeVisible();
+
+  const user = userEvent.setup();
+  const showButton = screen.getByText('Show more');
+  await user.click(showButton);
+
+  expect(url).toBeVisible();
+  expect(likes).toBeVisible();
 });
