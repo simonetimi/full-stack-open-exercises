f4fd34e03e7e694550baf30d7d133fe0da415853
Simone <150595274+simonetimi@users.noreply.github.com>
2024-03-17 10:33:47 +0100

    Complete exercises from 6.6 to 6.8

---
+++ part6/redux-anecdotes/src/AnecdoteForm.jsx
@@ -0,0 +1,25 @@
+import { useDispatch } from 'react-redux';
+import { addAnecdote } from './reducers/anecdoteReducer';
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch();
+  const add = (event) => {
+    event.preventDefault();
+    const content = event.target.newAnecdote.value;
+    event.target.newAnecdote.value = '';
+    dispatch(addAnecdote(content));
+  };
+  return (
+    <>
+      <h2>create new</h2>
+      <form onSubmit={add}>
+        <div>
+          <input name="newAnecdote" />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </>
+  );
+};
+
+export default AnecdoteForm;
--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,41 +1,13 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { addAnecdote, voteThis } from './reducers/anecdoteReducer';
+import AnecdoteForm from './AnecdoteForm';
+import AnecdoteList from './AnecdoteList';
 
 const App = () => {
-  const anecdotes = useSelector((state) => state.sort((a, b) => b.votes - a.votes));
-  const dispatch = useDispatch();
-
-  const vote = (id) => {
-    dispatch(voteThis(id));
-  };
-
-  const add = (event) => {
-    event.preventDefault();
-    const content = event.target.newAnecdote.value;
-    event.target.newAnecdote.value = '';
-    dispatch(addAnecdote(content));
-  };
-
   return (
-    <div>
+    <>
       <h2>Anecdotes</h2>
-      {anecdotes.map((anecdote) => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      ))}
-      <h2>create new</h2>
-      <form onSubmit={add}>
-        <div>
-          <input name="newAnecdote" />
-        </div>
-        <button type="submit">create</button>
-      </form>
-    </div>
+      <AnecdoteList />
+      <AnecdoteForm />
+    </>
   );
 };
 
