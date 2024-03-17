efd423f51dd1317e6495827b86e9ec74bef9bcf4
Simone <150595274+simonetimi@users.noreply.github.com>
2024-03-17 21:19:56 +0100

    Complete exercise 6.20

--- part6/README.md
+++ part6/README.md
@@ -3,4 +3,5 @@
 ### Exercises
 
 - unicafe-redux: 6.1-6.2
-- redux-anecdotes 6.3 - 6.8
+- redux-anecdotes 6.3 - 6.19
+- query-anecdotes 6.20 - 6.24
---
+++ part6/query-anecdotes/src/App.jsx
@@ -0,0 +1,41 @@
+import AnecdoteForm from './components/AnecdoteForm';
+import Notification from './components/Notification';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+const App = () => {
+  const handleVote = (anecdote) => {
+    console.log('vote');
+  };
+
+  const anecdotes = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: () => axios.get('http://localhost:3001/anecdotes').then((res) => res.data),
+    retry: 1,
+  });
+
+  if (anecdotes.isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+
+      <Notification />
+      <AnecdoteForm />
+
+      {anecdotes.data.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
