b79c45c28b7196259236d05dbc82e5a25361bf69
Simone <150595274+simonetimi@users.noreply.github.com>
2024-03-26 21:00:05 +0100

    Complete exercise 9.23

--- part9/patientor-frontend/src/components/PatientDetailsPage/index.tsx
+++ part9/patientor-frontend/src/components/PatientDetailsPage/index.tsx
@@ -17,20 +17,59 @@ const PatientDetailPage = () => {
     })();
   });
 
+  if (!patient) {
+    return (
+      <div>
+        {' '}
+        <Typography align="center" variant="h6" marginBottom={'40px'}>
+          Patient Details
+        </Typography>
+        <Typography variant="body1" marginBottom={'40px'}>
+          Patient not found
+        </Typography>
+      </div>
+    );
+  }
+
   return (
     <div>
       <Typography align="center" variant="h6" marginBottom={'40px'}>
         Patient Details
       </Typography>
       <Typography variant="body1" marginBottom={'40px'}>
-        Display patient details for ID: {patient?.name}
+        Display patient details for ID: {patient.name}
       </Typography>
-      <Typography variant="body2">Name: {patient?.name}</Typography>
-      <Typography variant="body2">Gender: {patient?.gender}</Typography>
+      <Typography variant="body2">Name: {patient.name}</Typography>
+      <Typography variant="body2">Gender: {patient.gender}</Typography>
       <Typography variant="body2">
-        Date of birth: {patient?.dateOfBirth}
+        Date of birth: {patient.dateOfBirth}
       </Typography>
-      <Typography variant="body2">Occupation: {patient?.occupation}</Typography>
+      <Typography variant="body2">Occupation: {patient.occupation}</Typography>
+      {patient.entries.length > 1 ? (
+        <>
+          <Typography variant="h6" marginTop={'40px'}>
+            Entries:
+          </Typography>
+          {patient.entries.map((entry) => {
+            return (
+              <div>
+                <Typography variant="body2">Date: {entry.date}</Typography>
+                <Typography variant="body2">{entry.description}</Typography>
+                {entry.diagnosisCodes ? (
+                  <>
+                    <Typography variant="body1" marginTop={'20px'}>
+                      Codes:
+                    </Typography>
+                    <Typography variant="body2">
+                      {entry.diagnosisCodes.map((code) => code)}
+                    </Typography>
+                  </>
+                ) : null}
+              </div>
+            );
+          })}
+        </>
+      ) : null}
     </div>
   );
 };
